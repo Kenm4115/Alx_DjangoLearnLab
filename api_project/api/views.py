3d7cf6badba35c6f705e49f09702082e95417abc
Kenm <kenm4115@gmail.com>
2024-11-24 16:10:17 +0300

    Modified api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 
