d5e7e1c53fb26d8917894ac6fdeca4482d4d3945
Kenm <kenm4115@gmail.com>
2024-11-24 16:14:27 +0300

    Modified api project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics.ListAPIView
+from rest_framework.viewsets.ModelViewSet
 
 
 class BookList(ListAPIView):
