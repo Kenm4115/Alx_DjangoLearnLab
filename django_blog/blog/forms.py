f8049d09a79d2372acebd63bccf05793ad45c175
Kenm <kenm4115@gmail.com>
2024-12-08 11:19:36 +0300

    Modified django blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 
+from .models import Post
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,3 +11,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,5 @@
 
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -11,3 +12,11 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 ]
 
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
